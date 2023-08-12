3d554639930b241abed2fe62b3ae06d61da1c9cd
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-08-12 11:01:24 -0300

    Classes & arrays

---
+++ class-11/angularApp11/src/app/components/tareas/tareas.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Tarea } from 'src/app/models/tareas';
+
+@Component({
+  selector: 'app-tareas',
+  templateUrl: './tareas.component.html',
+  styleUrls: ['./tareas.component.css']
+})
+
+export class TareasComponent implements OnInit {
+
+  // Luego de definir la clase Tarea en tareas.ts, vamos a definir 
+  // una lista de tareas
+  listaTareas: Tarea[] = [];
+  nombreTarea: string = "";
+
+  constructor(){ }
+
+  ngOnInit(): void { }
+
+  agregarTarea(){
+    const tarea:Tarea = {
+      nombre: this.nombreTarea,
+      estado: false
+    }
+
+    // Agrego la tarea a la lista de tareas
+    this.listaTareas.push(tarea);
+
+    // una vez que se agrego la tarea a la lista, se resetea el nombre de la tarea a vacio
+    this.nombreTarea="";
+  }
+
+}

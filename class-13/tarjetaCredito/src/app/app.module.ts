03bff2e04f3e3fa4e0b51cdefe4d6b3a1e413ad0
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-08-13 19:50:20 -0300

    Reactive forms

--- class-13/tarjetaCredito/src/app/app.module.ts
+++ class-13/tarjetaCredito/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrearTarjetaComponent } from './components/crear-tarjeta/crear-tarjeta.component';
 import { ListarTarjetaComponent } from './components/listar-tarjeta/listar-tarjeta.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ListarTarjetaComponent } from './components/listar-tarjeta/listar-tarje
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- class-13/tarjetaCredito/src/app/components/crear-tarjeta/crear-tarjeta.component.ts
+++ class-13/tarjetaCredito/src/app/components/crear-tarjeta/crear-tarjeta.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-tarjeta',
   templateUrl: './crear-tarjeta.component.html',
   styleUrls: ['./crear-tarjeta.component.css']
 })
-export class CrearTarjetaComponent {
+export class CrearTarjetaComponent implements OnInit {
+
+  forms:FormGroup; // importada de la clase ReactiveFormsModule en app.module.ts
+
+  constructor(private fb:FormBuilder){
+    this.forms = this.fb.group({
+      // elegimos .group porque el componente tiene un grupo de datos que queremos agrupar
+      titular:['', Validators.required],
+      nroTarjeta:['', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]],
+      fechaExpiracion:['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
+      cvv:['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]]
+    })
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  crearTarjeta(){
+    console.log(this.forms);
+  }
 
 }
